a6f8e91e12645201a644ff75c35cc5fc21111089
sfasasa <ejmilli03@gmail.com>
2025-07-07 17:25:06 +0300

    ..

--- Next + React + Typescript/app/api/categories/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const res = await fetch("http://localhost:8080/api/categories");
-  const data = await res.json();
-  return NextResponse.json(data);
-}
--- Next + React + Typescript/components/PanelRight.tsx
+++
@@ -1,8 +0,0 @@
-const PanelRight = () => (
-  <aside id="right-panel">
-    <h2>Users</h2>
-    <ul id="user-list">{/* User list rendering goes here */}</ul>
-  </aside>
-);
-
-export default PanelRight;
